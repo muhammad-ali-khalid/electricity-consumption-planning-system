544c235794fe3fad5f9f875143bd190ac5cd68a9
Haider-Khan12 <151368345+Haider-Khan12@users.noreply.github.com>
2024-12-05 18:43:08 +0500

    Update Plan.h

--- Plan.h
+++ Plan.h
@@ -10,163 +10,224 @@ namespace ECPS {
     using namespace System::Drawing;
     using namespace System::Data::SqlClient;
 
-    public ref class Plan : public System::Windows::Forms::Form
-    {
-    public:
-        Plan(void)
-        {
-            InitializeComponent();
 
-        }
 
-    protected:
-        ~Plan()
-        {
-            if (components)
-            {
-                delete components;
-            }
-        }
+	/// <summary>
+	/// Summary for Plan
+	/// </summary>
+	public ref class Plan : public System::Windows::Forms::Form
+	{
+	public:
+		User^ user = nullptr;
+		Plan(User^ user)
+		{
+			this->user = user;
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+			try {
+				String^ conn_str = "Data Source=(localdb)\\ECPS;Initial Catalog=ECPS_db;Integrated Security=True";
+				SqlConnection sqlConn(conn_str);
+				sqlConn.Open();
 
-    private:
-        System::Windows::Forms::Panel^ panelHeader;
-        System::Windows::Forms::Label^ labelTitle;
-        System::Windows::Forms::PictureBox^ pictureBoxIcon;
-        System::Windows::Forms::DataGridView^ dataGridViewPlan;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn1;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn2;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn3;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn4;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn5;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn6;
-    private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn7;
+				String^ query = "SELECT t1.Appliance, t1.Priority, t1.no_of_hours, t1.time, t1.Expense from plan_data as t1 Where Id = @id";
+				SqlCommand^ command = gcnew SqlCommand(query, % sqlConn);
+				command->Parameters->AddWithValue("@id", user->id);
 
-        // Required designer variable.
-        System::ComponentModel::Container^ components;
+
+				SqlDataAdapter^ adapter = gcnew SqlDataAdapter(command);
+				DataTable^ dataTable = gcnew DataTable();
+				adapter->Fill(dataTable);
+				dataGridView1->DataSource = dataTable;
+			}
+			catch (Exception^ e) {
+				MessageBox::Show(e->Message);
+			}
+
+			try {
+				String^ conn_str = "Data Source=(localdb)\\ECPS;Initial Catalog=ECPS_db;Integrated Security=True";
+				SqlConnection sqlConn(conn_str);
+				sqlConn.Open();
+
+				String^ query = "SELECT t1.Appliance, t1.Priority, t1.no_of_hours, t1.time, (t1.Expense * 30) as Monthly_expenses from plan_data as t1 Where Id = @id";
+				SqlCommand^ command = gcnew SqlCommand(query, % sqlConn);
+				command->Parameters->AddWithValue("@id", user->id);
+
+
+				SqlDataAdapter^ adapter = gcnew SqlDataAdapter(command);
+				DataTable^ dataTable = gcnew DataTable();
+				adapter->Fill(dataTable);
+				dataGridView2->DataSource = dataTable;
+			}
+			catch (Exception^ e) {
+				MessageBox::Show(e->Message);
+			}
+
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Plan()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^ panel2;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+
+
+
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+
+
+
+
+
+
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
-        void InitializeComponent(void)
-        {
-            System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Plan::typeid));
-            this->panelHeader = (gcnew System::Windows::Forms::Panel());
-            this->labelTitle = (gcnew System::Windows::Forms::Label());
-            this->pictureBoxIcon = (gcnew System::Windows::Forms::PictureBox());
-            this->dataGridViewPlan = (gcnew System::Windows::Forms::DataGridView());
-            this->dataGridViewTextBoxColumn1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-            this->dataGridViewTextBoxColumn2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-            this->dataGridViewTextBoxColumn3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-            this->dataGridViewTextBoxColumn4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-            this->dataGridViewTextBoxColumn5 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-            this->dataGridViewTextBoxColumn6 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-            this->dataGridViewTextBoxColumn7 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-
-            this->panelHeader->SuspendLayout();
-            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBoxIcon))->BeginInit();
-            (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewPlan))->BeginInit();
-            this->SuspendLayout();
-
-            // 
-            // panelHeader
-            // 
-            this->panelHeader->BackColor = System::Drawing::Color::White;
-            this->panelHeader->Controls->Add(this->labelTitle);
-            this->panelHeader->Controls->Add(this->pictureBoxIcon);
-            this->panelHeader->Location = System::Drawing::Point(303, 12);
-            this->panelHeader->Name = L"panelHeader";
-            this->panelHeader->Size = System::Drawing::Size(243, 80);
-            this->panelHeader->TabIndex = 0;
-
-            // 
-            // labelTitle
-            // 
-            this->labelTitle->AutoSize = true;
-            this->labelTitle->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium", 20, System::Drawing::FontStyle::Bold));
-            this->labelTitle->Location = System::Drawing::Point(80, 20);
-            this->labelTitle->Name = L"labelTitle";
-            this->labelTitle->Size = System::Drawing::Size(156, 34);
-            this->labelTitle->TabIndex = 1;
-            this->labelTitle->Text = L"Usage Plan";
-            this->labelTitle->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-
-            // 
-            // pictureBoxIcon
-            // 
-            this->pictureBoxIcon->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBoxIcon.Image")));
-            this->pictureBoxIcon->Location = System::Drawing::Point(10, 15);
-            this->pictureBoxIcon->Name = L"pictureBoxIcon";
-            this->pictureBoxIcon->Size = System::Drawing::Size(60, 50);
-            this->pictureBoxIcon->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-            this->pictureBoxIcon->TabIndex = 0;
-            this->pictureBoxIcon->TabStop = false;
-
-            // 
-            // dataGridViewPlan
-            // 
-            this->dataGridViewPlan->AllowUserToAddRows = false;
-            this->dataGridViewPlan->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
-            this->dataGridViewPlan->AutoSizeRowsMode = System::Windows::Forms::DataGridViewAutoSizeRowsMode::AllCells;
-            this->dataGridViewPlan->BackgroundColor = System::Drawing::Color::White;
-            this->dataGridViewPlan->RowHeadersVisible = false; // Disable row headers for cleaner appearance
-            this->dataGridViewPlan->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(7) {
-                this->dataGridViewTextBoxColumn1,
-                    this->dataGridViewTextBoxColumn2, this->dataGridViewTextBoxColumn3, this->dataGridViewTextBoxColumn4, this->dataGridViewTextBoxColumn5,
-                    this->dataGridViewTextBoxColumn6, this->dataGridViewTextBoxColumn7
-            });
-            this->dataGridViewPlan->Location = System::Drawing::Point(50, 120);
-            this->dataGridViewPlan->Name = L"dataGridViewPlan";
-            this->dataGridViewPlan->Size = System::Drawing::Size(759, 300); // Increase grid height for better row visibility
-            this->dataGridViewPlan->TabIndex = 2;
-
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this->dataGridViewTextBoxColumn1->HeaderText = L"Appliance Name";
-            this->dataGridViewTextBoxColumn1->Name = L"dataGridViewTextBoxColumn1";
-
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this->dataGridViewTextBoxColumn2->HeaderText = L"Wattage (W)";
-            this->dataGridViewTextBoxColumn2->Name = L"dataGridViewTextBoxColumn2";
-
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this->dataGridViewTextBoxColumn3->HeaderText = L"Usage Timing";
-            this->dataGridViewTextBoxColumn3->Name = L"dataGridViewTextBoxColumn3";
-
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this->dataGridViewTextBoxColumn4->HeaderText = L"Priority";
-            this->dataGridViewTextBoxColumn4->Name = L"dataGridViewTextBoxColumn4";
-
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this->dataGridViewTextBoxColumn5->HeaderText = L"Peak Hour Usage";
-            this->dataGridViewTextBoxColumn5->Name = L"dataGridViewTextBoxColumn5";
-
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this->dataGridViewTextBoxColumn6->HeaderText = L"Status";
-            this->dataGridViewTextBoxColumn6->Name = L"dataGridViewTextBoxColumn6";
-
-            // 
-            // dataGridViewTextBoxColumn7
-            // 
-            this->dataGridViewTextBoxColumn7->HeaderText = L"Estimated Cost (PKR)";
-            this->dataGridViewTextBoxColumn7->Name = L"dataGridViewTextBoxColumn7";
-
-            // Add rows for appliances
-            this->dataGridViewPlan->Rows->Add(L"Air Conditioner", L"1200W", L"9 PM - 6 AM", L"High", L"Yes", L"ON", L"57.60");
-            this->dataGridViewPlan->Rows->Add(L"Lights", L"20W", L"6 PM - 11 PM", L"High", L"Yes", L"ON", L"5.00");
-            this->dataGridViewPlan->Rows->Add(L"Water Pump", L"750W", L"Non-peak hours", L"Medium", L"No", L"OFF", L"0.00");
-            this->dataGridViewPlan->Rows->Add(L"TV", L"100W", L"Non-peak hours", L"Low", L"No", L"OFF", L"0.00");
-            this->dataGridViewPlan->Rows->Add(L"Washing Machine", L"500W", L"10 AM - 12 PM", L"Medium", L"No", L"OFF", L"10.00");
-            this->dataGridViewPlan->Rows->Add(L"Fan", L"60W", L"24 Hours", L"High", L"Yes", L"ON", L"8.64");
-
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Plan::typeid));
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->panel2->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// panel2
+			// 
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->pictureBox2);
+			this->panel2->Location = System::Drawing::Point(901, 72);
+			this->panel2->Margin = System::Windows::Forms::Padding(6);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(516, 131);
+			this->panel2->TabIndex = 11;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(136, 23);
+			this->label1->Margin = System::Windows::Forms::Padding(12, 0, 12, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(353, 81);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Usage Plan";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->label1->Click += gcnew System::EventHandler(this, &Plan::label1_Click);
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(6, 23);
+			this->pictureBox2->Margin = System::Windows::Forms::Padding(6);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(112, 88);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 1;
+			this->pictureBox2->TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(92, 464);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->ReadOnly = true;
+			this->dataGridView1->RowHeadersWidth = 82;
+			this->dataGridView1->Size = System::Drawing::Size(996, 424);
+			this->dataGridView1->TabIndex = 15;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::DarkOrange;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Georgia", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(109, 1235);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(214, 81);
+			this->button1->TabIndex = 16;
+			this->button1->Text = L"Dashboard";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Plan::button1_Click);
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Location = System::Drawing::Point(1256, 464);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->ReadOnly = true;
+			this->dataGridView2->RowHeadersWidth = 82;
+			this->dataGridView2->Size = System::Drawing::Size(996, 424);
+			this->dataGridView2->TabIndex = 17;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(95, 323);
+			this->label2->Margin = System::Windows::Forms::Padding(12, 0, 12, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(319, 81);
+			this->label2->TabIndex = 18;
+			this->label2->Text = L"Daily Plan";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(1251, 308);
+			this->label3->Margin = System::Windows::Forms::Padding(12, 0, 12, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(405, 81);
+			this->label3->TabIndex = 19;
+			this->label3->Text = L"Monthly Plan";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
             // 
             // Plan
             // 
