d56c38f842eadd9e52949403903fcd024503a04b
Haider-Khan12 <151368345+Haider-Khan12@users.noreply.github.com>
2024-12-06 22:14:01 +0500

    Update SignUp.h

--- SignUp.h
+++ SignUp.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "User.h"
+#include "Dashboard.h"
+
 
 namespace ECPS {
 
@@ -68,6 +70,9 @@ namespace ECPS {
 	private: System::Windows::Forms::Panel^ panel3;
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Panel^ panel8;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label3;
 
 	private:
 		/// <summary>
@@ -101,6 +106,9 @@ namespace ECPS {
 			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel8 = (gcnew System::Windows::Forms::Panel());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -111,6 +119,7 @@ namespace ECPS {
 			this->panel6->SuspendLayout();
 			this->panel7->SuspendLayout();
 			this->panel1->SuspendLayout();
+			this->panel8->SuspendLayout();
 			this->panel3->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -119,7 +128,7 @@ namespace ECPS {
 			this->panel2->Controls->Add(this->label1);
 			this->panel2->Controls->Add(this->pictureBox1);
 			this->panel2->Location = System::Drawing::Point(700, 23);
-			this->panel2->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel2->Margin = System::Windows::Forms::Padding(6);
 			this->panel2->Name = L"panel2";
 			this->panel2->Size = System::Drawing::Size(1298, 312);
 			this->panel2->TabIndex = 10;
@@ -141,7 +150,7 @@ namespace ECPS {
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
 			this->pictureBox1->Location = System::Drawing::Point(572, 127);
-			this->pictureBox1->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(6);
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->Size = System::Drawing::Size(152, 154);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
@@ -166,7 +175,7 @@ namespace ECPS {
 			this->panel4->Controls->Add(this->textBox4);
 			this->panel4->Controls->Add(this->label6);
 			this->panel4->Location = System::Drawing::Point(20, 144);
-			this->panel4->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel4->Margin = System::Windows::Forms::Padding(6);
 			this->panel4->Name = L"panel4";
 			this->panel4->Size = System::Drawing::Size(730, 135);
 			this->panel4->TabIndex = 14;
@@ -177,7 +186,7 @@ namespace ECPS {
 			this->textBox4->Font = (gcnew System::Drawing::Font(L"Calibri", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox4->Location = System::Drawing::Point(32, 62);
-			this->textBox4->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->textBox4->Margin = System::Windows::Forms::Padding(6);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size(688, 44);
 			this->textBox4->TabIndex = 6;
@@ -199,7 +208,7 @@ namespace ECPS {
 			this->panel5->Controls->Add(this->textBox5);
 			this->panel5->Controls->Add(this->label5);
 			this->panel5->Location = System::Drawing::Point(20, 290);
-			this->panel5->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel5->Margin = System::Windows::Forms::Padding(6);
 			this->panel5->Name = L"panel5";
 			this->panel5->Size = System::Drawing::Size(730, 125);
 			this->panel5->TabIndex = 15;
@@ -209,7 +218,7 @@ namespace ECPS {
 			this->textBox5->Font = (gcnew System::Drawing::Font(L"Calibri", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox5->Location = System::Drawing::Point(32, 62);
-			this->textBox5->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->textBox5->Margin = System::Windows::Forms::Padding(6);
 			this->textBox5->Name = L"textBox5";
 			this->textBox5->Size = System::Drawing::Size(688, 44);
 			this->textBox5->TabIndex = 6;
@@ -231,7 +240,7 @@ namespace ECPS {
 			this->panel6->Controls->Add(this->textBox6);
 			this->panel6->Controls->Add(this->label7);
 			this->panel6->Location = System::Drawing::Point(20, 427);
-			this->panel6->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel6->Margin = System::Windows::Forms::Padding(6);
 			this->panel6->Name = L"panel6";
 			this->panel6->Size = System::Drawing::Size(730, 125);
 			this->panel6->TabIndex = 16;
@@ -241,7 +250,7 @@ namespace ECPS {
 			this->textBox6->Font = (gcnew System::Drawing::Font(L"Calibri", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox6->Location = System::Drawing::Point(32, 62);
-			this->textBox6->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->textBox6->Margin = System::Windows::Forms::Padding(6);
 			this->textBox6->Name = L"textBox6";
 			this->textBox6->Size = System::Drawing::Size(688, 44);
 			this->textBox6->TabIndex = 6;
@@ -263,7 +272,7 @@ namespace ECPS {
 			this->panel7->Controls->Add(this->textBox7);
 			this->panel7->Controls->Add(this->label8);
 			this->panel7->Location = System::Drawing::Point(20, 563);
-			this->panel7->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel7->Margin = System::Windows::Forms::Padding(6);
 			this->panel7->Name = L"panel7";
 			this->panel7->Size = System::Drawing::Size(730, 135);
 			this->panel7->TabIndex = 17;
@@ -273,7 +282,7 @@ namespace ECPS {
 			this->textBox7->Font = (gcnew System::Drawing::Font(L"Calibri", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox7->Location = System::Drawing::Point(32, 62);
-			this->textBox7->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->textBox7->Margin = System::Windows::Forms::Padding(6);
 			this->textBox7->Name = L"textBox7";
 			this->textBox7->Size = System::Drawing::Size(688, 44);
 			this->textBox7->TabIndex = 6;
@@ -299,10 +308,10 @@ namespace ECPS {
 			this->button2->Font = (gcnew System::Drawing::Font(L"Georgia", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button2->ForeColor = System::Drawing::Color::Black;
-			this->button2->Location = System::Drawing::Point(56, 737);
-			this->button2->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->button2->Location = System::Drawing::Point(1021, 1281);
+			this->button2->Margin = System::Windows::Forms::Padding(6);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(692, 75);
+			this->button2->Size = System::Drawing::Size(692, 57);
 			this->button2->TabIndex = 18;
 			this->button2->Text = L"Sign Up";
 			this->button2->UseVisualStyleBackColor = false;
@@ -310,24 +319,55 @@ namespace ECPS {
 			// 
 			// panel1
 			// 
+			this->panel1->Controls->Add(this->panel8);
 			this->panel1->Controls->Add(this->panel3);
-			this->panel1->Controls->Add(this->button2);
 			this->panel1->Controls->Add(this->panel4);
 			this->panel1->Controls->Add(this->panel7);
 			this->panel1->Controls->Add(this->panel5);
 			this->panel1->Controls->Add(this->panel6);
 			this->panel1->Location = System::Drawing::Point(966, 429);
-			this->panel1->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel1->Margin = System::Windows::Forms::Padding(6);
 			this->panel1->Name = L"panel1";
 			this->panel1->Size = System::Drawing::Size(786, 840);
 			this->panel1->TabIndex = 19;
+			// 
+			// panel8
+			// 
+			this->panel8->Controls->Add(this->comboBox1);
+			this->panel8->Controls->Add(this->label3);
+			this->panel8->Location = System::Drawing::Point(20, 705);
+			this->panel8->Margin = System::Windows::Forms::Padding(6);
+			this->panel8->Name = L"panel8";
+			this->panel8->Size = System::Drawing::Size(730, 135);
+			this->panel8->TabIndex = 18;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Lahore", L"Islamabad", L"Karachi" });
+			this->comboBox1->Location = System::Drawing::Point(35, 70);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(669, 33);
+			this->comboBox1->TabIndex = 3;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Franklin Gothic Medium", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(24, 15);
+			this->label3->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(214, 37);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Enter Location";
 			// 
 			// panel3
 			// 
 			this->panel3->Controls->Add(this->textBox3);
 			this->panel3->Controls->Add(this->label2);
 			this->panel3->Location = System::Drawing::Point(20, 6);
-			this->panel3->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->panel3->Margin = System::Windows::Forms::Padding(6);
 			this->panel3->Name = L"panel3";
 			this->panel3->Size = System::Drawing::Size(730, 127);
 			this->panel3->TabIndex = 13;
@@ -337,7 +377,7 @@ namespace ECPS {
 			this->textBox3->Font = (gcnew System::Drawing::Font(L"Calibri", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->textBox3->Location = System::Drawing::Point(32, 62);
-			this->textBox3->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->textBox3->Margin = System::Windows::Forms::Padding(6);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(688, 44);
 			this->textBox3->TabIndex = 6;
@@ -363,9 +403,10 @@ namespace ECPS {
 			this->BackColor = System::Drawing::Color::White;
 			this->ClientSize = System::Drawing::Size(2564, 1399);
 			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->panel2);
-			this->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
+			this->Margin = System::Windows::Forms::Padding(6);
 			this->Name = L"SignUp";
 			this->Text = L"SignUp";
 			this->Load += gcnew System::EventHandler(this, &SignUp::SignUp_Load);
@@ -381,6 +422,8 @@ namespace ECPS {
 			this->panel7->ResumeLayout(false);
 			this->panel7->PerformLayout();
 			this->panel1->ResumeLayout(false);
+			this->panel8->ResumeLayout(false);
+			this->panel8->PerformLayout();
 			this->panel3->ResumeLayout(false);
 			this->panel3->PerformLayout();
 			this->ResumeLayout(false);
@@ -396,6 +439,7 @@ namespace ECPS {
 	}
 	private: System::Void label8_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
+	public: User^ user = gcnew User();
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	//signup here
 		String^ username = textBox3->Text;
@@ -403,6 +447,7 @@ namespace ECPS {
 		String^ contact_no = textBox5->Text;
 		String^ Id = textBox6->Text;
 		String^ password = textBox7->Text;
+		String^ location = comboBox1->Text;
 
 		//bool username_validation = IsFirstCharacterAlphabet(username);
 		//bool income_validation = ContainsDigit(income);
@@ -420,7 +465,7 @@ namespace ECPS {
 
 				sqlConn.Open();
 
-				String^ sqlQuery = "INSERT INTO Credentials (Name , Password , Id , Contact_no , Age) VALUES (@user  , @pwd , @id , @contact , @age)";
+				String^ sqlQuery = "INSERT INTO Credentials (Name , Password , Id , Contact_no , Age, Location, Budget , Units_range , Expenses) VALUES (@user  , @pwd , @id , @contact , @age, @loc, @budget , @range , @expense)";
 
 				SqlCommand^ command = gcnew SqlCommand(sqlQuery, % sqlConn);
 				command->Parameters->AddWithValue("@user", username);
@@ -428,6 +473,10 @@ namespace ECPS {
 				command->Parameters->AddWithValue("@id", Id);
 				command->Parameters->AddWithValue("@contact", contact_no);
 				command->Parameters->AddWithValue("@age", age);
+				command->Parameters->AddWithValue("@loc", location);
+				command->Parameters->AddWithValue("@range", user->unit_range);
+				command->Parameters->AddWithValue("@budget", user->budget);
+				command->Parameters->AddWithValue("@expense", user->expenses);
 
 				command->ExecuteNonQuery();
 
@@ -437,16 +486,22 @@ namespace ECPS {
 				user->Contact_no = contact_no;
 				user->Age = age;
 				user->password = password;
+				user->location = location;
 
 				this->Close();
 
+				Dashboard^ dash = gcnew Dashboard(user);
+				dash->WindowState = System::Windows::Forms::FormWindowState::Maximized;
+				dash->ShowDialog();
+
 			}
 			catch (Exception^ e) {
-				MessageBox::Show("Failed to Create Account", "Account Creation Failed", MessageBoxButtons::OK);
+				MessageBox::Show(e->Message, "Account Creation Failed", MessageBoxButtons::OK);
 			}
 
 		}
 	}
+
 	private: System::Void panel4_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
 	}
 	};
